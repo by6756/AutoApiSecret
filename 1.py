2aaae4a711caf073d877ce1369aa7ebd8f9c6697
by6756 <by6756@hotmail.com>
2021-01-09 16:46:56 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -76,7 +76,10 @@ def main():
         if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',headers=headers).status_code == 200:
             num1+=1
             print('10调用成功'+str(num1)+'次')
-            print('此次运行结束时间为 :', localtime)
+        if req.get(r'https://graph.microsoft.com/v1.0/sites/root',headers=headers).status_code == 200:
+            num1+=1
+            print('11调用成功'+str(num1)+'次')
+            print('此次运行结束时间为 :', localtime)  
     except:
         print("pass")
         pass
